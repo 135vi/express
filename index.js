a3c2c0bbbbd40634727b1e68c1d2edda5b4c28fd
Гулова Виталия <gulovavitalia@gmail.com>
2024-03-14 21:03:06 +0300

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const friendsRouter = require('./routers/friends.router');
+const usersRouter = require('./routers/users.router');
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use('/friends', friendsRouter);
+app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
